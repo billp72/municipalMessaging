50bad8038a904c0c3c771eae0490a594d0e06902
billpope <epop.bill@gmail.com>
2023-03-22 12:18:57 -0400

    ui homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_grid/responsive_grid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,18 +49,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  //void _incrementCounter() {
+  // setState(() {
+  // This call to setState tells the Flutter framework that something has
+  // changed in this State, which causes it to rerun the build method below
+  // so that the display can reflect the updated values. If we changed
+  // _counter without calling setState(), then the build method would not be
+  // called again, and so nothing would appear to happen.
+  // _counter++;
+  // });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -70,46 +71,82 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Municipal Message'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.20), BlendMode.dstATop),
+                image: const AssetImage('assets/images/gov.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+                child: ResponsiveGridRow(children: [
+              ResponsiveGridCol(
+                xs: 12,
+                child: Container(
+                  height: 200,
+                  alignment: const Alignment(0, 0),
+                  //color: Colors.blue,
+                  child: const Text(""),
+                ),
+              ),
+              ResponsiveGridCol(
+                lg: 12,
+                child: Container(
+                  height: 80,
+                  alignment: const Alignment(0, 0),
+                  child: Text(
+                      'Welcome to Home Cookn\', a place to order home-cooked meals and have them delivered directly from their home to users. Peruse home cooks in your area to see what\'s on the menu for today.',
+                      maxLines: 10,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          height: 1.3,
+                          color: Colors.grey[600],
+                          fontStyle: FontStyle.italic)),
+                ),
+              ),
+              ResponsiveGridCol(
+                xs: 6,
+                md: 3,
+                child: Container(
+                  height: 100,
+                  alignment: const Alignment(0, 0),
+                  //color: Colors.green,
+                  child: ElevatedButton(
+                    child: const Text('SIGN UP', style: TextStyle(fontSize: 15.0)),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(
+                        '/cook',
+                        arguments: 'Hello new cook!',
+                      );
+                    },
+                  ),
+                ),
+              ),
+              ResponsiveGridCol(
+                xs: 6,
+                md: 3,
+                child: Container(
+                  height: 100,
+                  alignment: const Alignment(0, 0),
+                  //color: Colors.orange,
+                  child: ElevatedButton(
+                    child: const Text('SIGN IN', style: TextStyle(fontSize: 15.0)),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(
+                        '/order',
+                        arguments: 'Find home cookn!',
+                      );
+                    },
+                  ),
+                ),
+              ),
+              /**/
+            ]))));
   }
 }
