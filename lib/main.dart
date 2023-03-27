31b0ff47ce343dffd6ff5ebb66c34b572566830f
billpope <epop.bill@gmail.com>
2023-03-27 10:11:39 -0400

    fetch FB

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'navigation/screens/login.dart';
 import 'navigation/screens/home.dart';
@@ -5,7 +8,26 @@ import 'navigation/screens/resident.dart';
 import 'navigation/screens/government.dart';
 import 'navigation/landing_page.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
+  HttpsCallable callable =
+      FirebaseFunctions.instance.httpsCallable('helloWorld');
+  try {
+    final result = await callable();
+
+    List msg = result.data;
+    // ignore: avoid_print
+    print(msg);
+  } catch (e) {
+    // ignore: avoid_print
+    print('$e ERROR FETCHING DATA');
+  }
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
