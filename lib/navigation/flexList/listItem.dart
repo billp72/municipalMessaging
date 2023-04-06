b2345dd0952e04f3f262804dd87bb361fa4e9aee
William Pope <epop.bill@gmail.com>
2023-04-06 14:47:47 -0400

    dynamic list view

---
+++ lib/navigation/flexList/listItem.dart
@@ -0,0 +1,44 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
+}
+
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: 
+        Theme.of(context).textTheme
+        .headlineSmall
+    );
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) => const SizedBox.shrink();
+}
+
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+
+  @override
+  Widget buildTitle(BuildContext context) => Text(sender, style: const TextStyle(
+                color: Colors.blue,
+              ));
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(body);
+}
