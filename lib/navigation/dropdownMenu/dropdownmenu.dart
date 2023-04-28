c2081d8b527a4fa54c858f8592ae2fa134a8f522
billpope <epop.bill@gmail.com>
2023-04-28 15:34:20 -0400

    dropdown server

--- lib/navigation/dropdownMenu/dropdownmenu.dart
+++ lib/navigation/dropdownMenu/dropdownmenu.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
+
+import '../../getUserFromPreference.dart';
 
 // ignore: must_be_immutable
 class MyDropdown extends StatefulWidget {
@@ -31,35 +34,37 @@ class MyDropdownState extends State<MyDropdown>
   final String selected;
   final Function(String, String) onSelectedValueChange;
   final Function() enable;
-  // ignore: prefer_typing_uninitialized_variables
 
   // ignore: prefer_typing_uninitialized_variables
   final drop;
-  // final state = LocalState();
-  // final HttpsCallable callable =
-  //     FirebaseFunctions.instance.httpsCallable('getUserAlerts');
-  // dynamic _username;
+  final state = LocalState();
+  final HttpsCallable cityCallable =
+      FirebaseFunctions.instance.httpsCallable('getCities');
+  final HttpsCallable stateCallable =
+      FirebaseFunctions.instance.httpsCallable('getStates');
 
   MyDropdownState(
       this.selected, this.drop, this.onSelectedValueChange, this.enable);
 
   Future<List<String>> createDdArray() async {
+    final username = await state.getMap("USER");
     List<String> thearray = [];
     if (selected == "state") {
-      thearray = [
-        'Select $selected',
-        'Maine',
-        'New Hamshire',
-        'New York',
-        'New Jersey',
-        'Pennsylvyania'
-      ];
+      final state =
+          await stateCallable.call(<String, dynamic>{'uid': username['uid']});
+      thearray = state.data;
+      //  [
+      //   'Select $selected',
+      //   'Maine',
+      //   'New Hamshire',
+      //   'New York',
+      //   'New Jersey',
+      //   'Pennsylvyania'
+      // ];
     } else if (selected == "city") {
-      // _username = await state.getMap("USER");
-      // final resp =
-      //     await callable.call(<String, dynamic>{'uid': _username['uid']});
-      // final data = resp.data;
-      thearray = ['Select $selected', 'Lisbon', 'Portland'];
+      final city =
+          await cityCallable.call(<String, dynamic>{'uid': username['uid']});
+      thearray = city.data; //['Select $selected', 'Lisbon', 'Portland'];
     } else if (selected == "frequency") {
       thearray = ['Select $selected', 'all', 'daily', 'weekly', 'monthly'];
     } else if (selected == "delivery") {
